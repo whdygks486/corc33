2f1737c157cfdf906c0a89f8025a97e0b6abe3c9
조요한 <whdygks486@naver.com>
2024-03-20 20:22:28 +0900

    Initial commit

---
+++ src/main/java/com/swig/manda/dto/DetailDto.java
@@ -0,0 +1,29 @@
+package com.swig.manda.dto;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class DetailDto {
+
+
+
+    @Id
+    private Long missionIndex;
+
+
+    private List<String> goalList;
+
+    private String UserId;
+
+    private List<String> goalText;
+
+}
